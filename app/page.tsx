f3969a49d5c3dafa4861461f35ca890e3ff6efeb
Shubhashit <shubhashitjain@gmail.com>
2024-07-04 17:38:54 +0530

    scoket io connection done

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,66 @@
 'use client'
 
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useDraw } from '../hooks/useDraw'
 import { ChromePicker } from 'react-color'
 
-interface pageProps {}
+import { io } from 'socket.io-client'
+import { drawLine } from '../utils/drawLine'
+const socket = io('http://localhost:3001')
 
-const page: FC<pageProps> = ({}) => {
+interface pageProps { }
+
+type DrawLineProps = {
+  prevPoint: Point | null
+  currentPoint: Point
+  color: string
+}
+
+const page: FC<pageProps> = ({ }) => {
   const [color, setColor] = useState<string>('#000')
-  const { canvasRef, onMouseDown, clear } = useDraw(drawLine)
+  const { canvasRef, onMouseDown, clear } = useDraw(createLine)
 
-  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
-    const { x: currX, y: currY } = currentPoint
-    const lineColor = color
-    const lineWidth = 5
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d')
 
-    let startPoint = prevPoint ?? currentPoint
-    ctx.beginPath()
-    ctx.lineWidth = lineWidth
-    ctx.strokeStyle = lineColor
-    ctx.moveTo(startPoint.x, startPoint.y)
-    ctx.lineTo(currX, currY)
-    ctx.stroke()
+    socket.emit('client-ready')
 
-    ctx.fillStyle = lineColor
-    ctx.beginPath()
-    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI)
-    ctx.fill()
+    socket.on('get-canvas-state', () => {
+      if (!canvasRef.current?.toDataURL()) return
+      console.log('sending canvas state')
+      socket.emit('canvas-state', canvasRef.current.toDataURL())
+    })
+
+    socket.on('canvas-state-from-server', (state: string) => {
+      console.log('I received the state')
+      const img = new Image()
+      img.src = state
+      img.onload = () => {
+        ctx?.drawImage(img, 0, 0)
+      }
+    })
+
+    socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLineProps) => {
+      if (!ctx) return console.log('no ctx here')
+      drawLine({ prevPoint, currentPoint, ctx, color })
+    })
+
+    socket.on('clear', clear)
+
+    return () => {
+      socket.off('draw-line')
+      socket.off('get-canvas-state')
+      socket.off('canvas-state-from-server')
+      socket.off('clear')
+    }
+  }, [canvasRef])
+
+  function createLine({ prevPoint, currentPoint, ctx }: Draw) {
+    socket.emit('draw-line', { prevPoint, currentPoint, color })
+    drawLine({ prevPoint, currentPoint, ctx, color })
   }
-
   return (
-    <div className='w-screen h-screen bg-white flex justify-center items-center'>
+    <div className='w-[90%] h-[100vh] bg-white flex flex-row justify-center items-center'>
       <div className='flex flex-col gap-10 pr-10'>
         <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
         <button type='button' className='p-2 rounded-md border border-black' onClick={clear}>
@@ -40,8 +70,8 @@ const page: FC<pageProps> = ({}) => {
       <canvas
         ref={canvasRef}
         onMouseDown={onMouseDown}
-        width={750}
-        height={750}
+        width={550}
+        height={550}
         className='border border-black rounded-md'
       />
     </div>
